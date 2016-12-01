0ff53f61d9faf624df3a048c11512ef6108a03d6
amine benselim <abens241@gmail.com>
2016-12-01 10:50:19 +0100

    fixed solution of day1 challenge 1

--- day1.js
+++ day1.js
@@ -2,43 +2,45 @@ const inputs = process.argv[2].split(", ");
 
 const dir = ['up','down','right','left'];
 var currentDir = dir[0];
-var lef = 0 , top = 0;
+var horizental = 0 , vertical = 0;
 
 inputs.forEach(function(v) {
+    let num = +v.split("").slice(1,v.length).join("");
+    console.log(num);
     switch (currentDir) {
         case 'up': 
             if (v[0] === 'R') {
-                lef += +v[1]; 
+                horizental += num; 
                 currentDir=dir[2];
             } else {
-                lef -= +v[1];
+                horizental -= num;
                 currentDir=dir[3];
             } 
             break;
         case 'down': 
             if (v[0] == 'L') {
-                lef += +v[1]; 
+                horizental += num; 
                 currentDir=dir[2];
             } else {
-                lef -= +v[1];
+                horizental -= num;
                 currentDir=dir[3];
             } 
             break;
         case 'left': 
             if (v[0] == 'R') {
-                top += +v[1]; 
+                vertical += num;
                 currentDir=dir[0];
             } else {
-                top -= +v[1];
+                vertical -= num;
                 currentDir=dir[1];
             }
             break;
         case 'right': 
             if(v[0] === 'L') {
-                top += +v[1]; 
+                vertical += num; 
                 currentDir=dir[0];
             } else {
-                top -= +v[1];
+                vertical -= num;
                 currentDir=dir[1];
             } 
             break;
@@ -46,6 +48,6 @@ inputs.forEach(function(v) {
             break;
     }     
 });
-console.log(lef);
-console.log(top);
-console.log(Math.abs(lef) + Math.abs(top));
+console.log(horizental);
+console.log(vertical);
+console.log(Math.abs(horizental) + Math.abs(vertical));
