d1ddc37020befb57257be717f4da80fc51380e9e
amine benselim <abens241@gmail.com>
2016-12-02 10:31:00 +0100

    "removed console logs from day 2 challenge 1"

--- day2.js
+++ day2.js
@@ -3,8 +3,6 @@ const lines = fs.readFileSync('day2.txt').toString().split("\n");
 
 console.log(lines.reduce((acc, ele, i)=> {
     let currentVal = (i === 0) ? 5 : acc[i-1];
-    console.log(acc);
-    console.log(i + ' ' + currentVal);
     console.log('new line is '+ ele);
     ele.split('').forEach((e) => {
         if(e === 'U' && currentVal > 3)  {currentVal -= 3; console.log('operation is '+ e + ' , move value to  '+ currentVal);} // it is not in the upper line of keypad
